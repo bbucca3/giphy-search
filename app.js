6ebcae69652aa231f9feca875bd388f3b856ef2a
Benjamin Bucca <benjamin.bucca3@gmail.com>
2018-02-09 20:09:28 -0500

    added giphy-api

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var http = require('http');
+var giphy = require('giphy-api')();
+
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -19,28 +21,33 @@ app.get('/greetings/:name', (req, res) => {
 
 app.get('/', (req, res) => {
   console.log(req.query);
-  let queryString = req.query.term;
-  let term = encodeURIComponent(queryString);
-  let url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
 
-  http.get(url, (response) => {
-    // set response encoding to utf8
-    response.setEncoding('utf8');
-
-    let body = '';
-
-    response.on('data', (d) => {
-      // stream data into body
-      body += d;
-    });
-
-    response.on('end', () => {
-      // when data is fully received parse into json
-      let parsed = JSON.parse(body);
-      // render home template and pass GIF data in to the tempalte
-      res.render('home', {gifs: parsed.data});
-    });
+  giphy.search(req.query.term, (err, response) => {
+    res.render('home', {gifs: response.data});
   });
+
+  // let queryString = req.query.term;
+  // let term = encodeURIComponent(queryString);
+  // let url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+  //
+  // http.get(url, (response) => {
+  //   // set response encoding to utf8
+  //   response.setEncoding('utf8');
+  //
+  //   let body = '';
+  //
+  //   response.on('data', (d) => {
+  //     // stream data into body
+  //     body += d;
+  //   });
+  //
+  //   response.on('end', () => {
+  //     // when data is fully received parse into json
+  //     let parsed = JSON.parse(body);
+  //     // render home template and pass GIF data in to the tempalte
+  //     res.render('home', {gifs: parsed.data});
+  //   });
+  // });
 });
 
 app.listen(3000, () => {
